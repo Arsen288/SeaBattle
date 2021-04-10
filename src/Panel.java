9edd05b9b04dff56113729a47da203321deb787f
Arsen228 <redrame2@gmail.com>
2021-04-10 16:18:13 +0300

    coment

--- src/Panel.java
+++ src/Panel.java
@@ -1,4 +1,3 @@
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,12 +9,11 @@ import javax.swing.Timer;
 public class Panel extends JPanel{
 	Game game;
 	Timer tm;
-	Font font;
 	Panel(){
 		setLayout(null);
 		game = new Game();
 		repaint();
-		 font  = new Font("sans-serif",2,40);
+		
 		JButton nGameBtn = new JButton("Ќова€ игра");
 		nGameBtn.setBounds(500, 700, 200, 50);
 		add(nGameBtn);
@@ -43,15 +41,5 @@ public class Panel extends JPanel{
 		super.paint(g);
 		game.computerField.paintField(g);
 		game.playerField.paintField(g);
-		if(game.computerWin()) {
-			int x = game.computerLeftIndent - game.centerIndent + 15;
-			int y =game.topIndent + game.fieldSize * game.cellSize + 50;
-			g.setFont(font);
-			g.drawString("YOU DEAD", x ,y);
-		}else if (game.playerWin()) {
-			int x = game.computerLeftIndent - game.centerIndent / 2 - 50;
-			int y =game.topIndent + game.fieldSize * game.cellSize + 50;
-			g.drawString("паздравл€ем ,вы сын маминой подруги ", x ,y);
-		}
 	}
 }
